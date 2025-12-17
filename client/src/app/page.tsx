cb38cd9ce0ff181304697ca32fd861dd0cf61ad1
Tejas Kamble <70898998+tejaskamble99@users.noreply.github.com>
2025-12-17 14:22:29 +0530

    Frontend started

---
+++ client/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
+      <h1 className="text-4xl font-bold">Task Manager Frontend 🚀</h1>
+    </main>
+
+  );
+}
